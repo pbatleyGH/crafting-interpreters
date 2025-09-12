253ff633af4b7508c47a8452fa9160c83e202083
Peter Batley <peter.batley@gmail.com>
2025-09-12 13:50:09 +0100

    add assignemnt

--- java-lox/com/craftinginterpreters/lox/Expr.java
+++ java-lox/com/craftinginterpreters/lox/Expr.java
@@ -3,6 +3,20 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    final Token name;
+    final Expr value;
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+  }
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -70,6 +84,7 @@ abstract class Expr {
     }
   }
   interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
